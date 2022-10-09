d57db17617dde44b7a61eb1b1c652104366d6b2e
Zerounary <song.c@sungeon.com>
2022-10-09 22:57:16 +0800

    feat: 集成 pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
+import * as Pinia from 'pinia';
 
 import 'virtual:windi.css'
 
 export function createApp() {
   const app = createSSRApp(App);
-  return {
-    app,
-  };
+	app.use(Pinia.createPinia());
+	return {
+		app,
+		Pinia,
+	};
 }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,23 @@
 <template>
   <view class="flex flex-col items-center justify-center">
     <image class="h-200rpx w-200rpx mt-200rpx mb-50rpx mx-auto" src="/static/logo.png" />
-    <view class="flex justify-center">
+    <view class="flex justify-center mb-50rpx">
       <text class=" text-36rpx text-hex-8f8f94">{{ title }}</text>
     </view>
+    <button @click="onClick">{{counter.count}}</button>
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useCounterStore } from '@/stores/counter';
+const counter = useCounterStore();
 const title = ref('Hello')
+
+const onClick = () => {
+  counter.increment();
+}
+
 </script>
 
 <style></style>
