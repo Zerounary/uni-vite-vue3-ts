fe5737e9a048f228668dc6234ac3cda289b81ffe
Zerounary <song.c@sungeon.com>
2022-10-09 21:51:31 +0800

    feat: support windicss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [WindiCSS(), uni()],
 });
