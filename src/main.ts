ac1ff8af5fbdfbb3d990c25b5878be41f6453035
Zerounary <song.c@sungeon.com>
2022-10-10 15:06:23 +0800

    feat: 添加条件编译，H5和APP支持 vant 组件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
+
 import * as Pinia from 'pinia';
 import piniaPersist from '@/plugins/pinia-plugin-persist'
 
+// #ifdef H5 || APP-PLUS
+import {Popup, Cell } from 'vant'
+import 'vant/lib/index.css'
+// #endif
 import 'virtual:windi.css'
 import './main.css'
 
@@ -11,6 +16,10 @@ export function createApp() {
 	const pinia = Pinia.createPinia();
 	pinia.use(piniaPersist)
 	app.use(pinia);
+	// #ifdef H5 || APP-PLUS
+	app.use(Popup)
+	app.use(Cell)
+	// #endif
 	return {
 		app,
 		Pinia,
